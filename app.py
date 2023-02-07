f979bc1b3c466b4f10e3e9d2abc5017fb4e447c2
Vincy Prince <124661000+RoyalRock2@users.noreply.github.com>
2023-02-07 10:50:52 +0000

    second

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
-from flask import Flask
+from flask import Flask,render_template,request,url_for
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template(template_name_or_list="index.html")
+
+
+@app.route('/display')
+def display():
+    msg = "This is your beautiful pic"
+    name = request.args["name"].strip().replace(" ","")
+    image = "yours.png"
+    if name.lower()=="bharath":
+        image = "mine.png"
+        name = ""
+        msg = "Get lost"
+
+
+
+    return render_template("display.html",name=name,image=image,msg=msg)
+
+
+if __name__ == '__main__':
+    app.run()
